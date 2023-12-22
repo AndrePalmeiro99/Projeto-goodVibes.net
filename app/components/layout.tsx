029a5b1bde16f6f0b64e92ce5318aac8220a4c67
André Palmeiro <andrepalmeiro14@gmail.com>
2023-12-22 12:19:37 -0300

    Mudanças no código

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -37,11 +37,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                                         Piadas
                                     </Link>
                                 </li>
-                                <li>
-                                    <Link href="/logado/imagens" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-                                        Imagens inspiradoras
-                                    </Link>
-                                </li>
                             </ul>
                         </div>
                     </div>
--- app/lib/ui/telaPiada.tsx
+++ app/lib/ui/telaPiada.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 import { useEffect, useState } from 'react';
 import { Piada } from '../domain/piadas';
